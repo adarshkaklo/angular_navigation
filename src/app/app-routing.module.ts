8e50a8d520bd473bcc3f5444fe2c873ed0a667d5
adarshkaklo <16mscit082@gmail.com>
2020-09-19 11:21:02 +0530

    navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path : 'about',
+    component : AboutComponent 
+  },
+  {
+    path : 'contact',
+    component :  ContactComponent
+  },
+  {
+    path : 'home',
+    component :  HomeComponent
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
